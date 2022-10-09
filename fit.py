7def55facda4ee22879571daaec383df9775b576
PhyEM_Chenyin <sunchenyin@umail.ustc.edu.cn>
2022-10-09 23:34:44 +0800

    1.0

---
+++ fit.py
@@ -0,0 +1,132 @@
+import numpy as np
+from scipy import optimize
+
+
+def exp_Fit_1D(x, data, fitRangeMin=None, fitRangeMax=None, fitInitial=None):
+    def target_func(x, a, b, c):
+        return a * np.exp(b*x)+c
+
+    # make a guess if fitInitial parameters are not available
+    if fitInitial is None:
+        if (data[-1] < data[0]) and (data[-1] + data[0])/2 > data[round(len(data) / 2)]:
+            fitInitial = [max(data)-min(data), -1.0 /
+                          x[round(len(x) / 2)], min(data)]
+        if (data[-1] > data[0]) and (data[-1] + data[0])/2 > data[round(len(data) / 2)]:
+            fitInitial = [max(data)-min(data), 1.0 /
+                          x[round(len(x) / 2)], min(data)]
+        if (data[-1] < data[0]) and (data[-1] + data[0])/2 < data[round(len(data) / 2)]:
+            fitInitial = [min(data)-max(data), +1.0 /
+                          x[round(len(x) / 2)], max(data)]
+        if (data[-1] > data[0]) and (data[-1] + data[0])/2 < data[round(len(data) / 2)]:
+            fitInitial = [min(data)-max(data), -1.0 /
+                          x[round(len(x) / 2)], max(data)]
+
+    parameters, covariance = optimize.curve_fit(
+        target_func, x, data, p0=fitInitial, bounds=(fitRangeMin, fitRangeMax), maxfev=500000)  # maxfev is the maximul times of iteration regression
+
+    err = np.sqrt(np.diag(covariance)) * 1.96  # 95% confidence area
+
+    return parameters, err
+
+
+def exp_FitX_2D(x, data, fitRangeMin=None, fitRangeMax=None, fitInitial=None):
+    parameters = []
+    err = []
+    for i in range(len(x)):
+        parameters_temp, err_temp = exp_Fit_1D(
+            x[i], data[i], fitRangeMin, fitRangeMax, fitInitial)
+        parameters.append(parameters_temp)
+        err.append(err_temp)
+    return parameters, err
+
+
+def exp_FitY_2D(y, data, fitRangeMin=None, fitRangeMax=None, fitInitial=None):
+    y = np.transpose(np.array(y)).tolist()
+    data = np.transpose(np.array(data)).tolist()
+    parameters, err = exp_FitX_2D(
+        y, data, fitRangeMin=None, fitRangeMax=None, fitInitial=None)
+    return parameters, err
+
+
+def linear_Fit_1D(x, data, fitRangeMin=None, fitRangeMax=None, fitInitial=None):
+    def target_func(x, a, b):
+        return a * x+b
+    if fitInitial is None:
+        fitInitial = [(data[-1]-data[0])/(x[-1]-x[0]), data[0] -
+                      (data[-1]-data[0])/(x[-1]-x[0])*x[0]]
+    parameters, covariance = optimize.curve_fit(
+        target_func, x, data, p0=fitInitial, bounds=(fitRangeMin, fitRangeMax), maxfev=500000)
+    err = np.sqrt(np.diag(covariance)) * 1.96  # 95% confidence area
+    return parameters, err
+
+
+def linear_FitX_2D(x, data, fitRangeMin=None, fitRangeMax=None, fitInitial=None):
+    parameters = []
+    err = []
+    for i in range(len(x)):
+        parameters_temp, err_temp = linear_Fit_1D(
+            x[i], data[i], fitRangeMin, fitRangeMax, fitInitial)
+        parameters.append(parameters_temp)
+        err.append(err_temp)
+    return parameters, err
+
+
+def linear_FitY_2D(y, data, fitRangeMin=None, fitRangeMax=None, fitInitial=None):
+    y = np.transpose(np.array(y)).tolist()
+    data = np.transpose(np.array(data)).tolist()
+    parameters, err = linear_FitX_2D(
+        y, data, fitRangeMin=None, fitRangeMax=None, fitInitial=None)
+    return parameters, err
+
+
+def guess_freq(x, y):
+    '''
+        Guess the frequency of input data
+        by finding sideband position in DFT of input data
+    '''
+    x = np.array(x)
+    y = np.array(y)
+    freq = np.fft.fftfreq(len(x), x[1] - x[0])  # uniform spacing
+    Y = abs(np.fft.fft(y))
+    # find peak (exclude zero frequency)
+    guess = abs(freq[np.argmax(Y[1:]) + 1])
+    return guess
+
+
+def sin_Fit_1D(x, data, fitRangeMin=None, fitRangeMax=None, fitInitial=None):
+    def target_fun(x, a, b, c, phi):
+        return a * np.sin(2*np.pi*b*x + phi) + c
+
+    # make a guess if initial parameters are not available
+    if p0 is None:
+        if np.argmax(data) == len(data) - 1:
+            freq = 1/np.pi/x[-1]
+        else:
+            freq = guess_freq(x, data)
+        amp = (np.max(data)-np.min(data))/2
+        offset = np.mean(data)
+        p0 = [amp, freq, offset, -np.pi/2]
+
+    parameters, covariance = optimize.curve_fit(
+        target_fun, x, data, p0=p0, maxfev=500000)
+    err = np.sqrt(np.diag(covariance)) * 1.96  # 95% confidence area
+    if err[1] > abs(parameters[1])*0.1:
+        errlist = []
+        parmlist = []
+        variance = []
+        for ii in range(5, len(data)):
+            p0[1] = x[ii]
+            parameters, covariance = optimize.curve_fit(
+                target_fun, x, data, p0=p0, maxfev=500000)  # maxfev修改拟合次数上限，默认为1000
+            err = np.sqrt(np.diag(covariance)) * 1.96  # 95% confidence area
+            errlist.append(err)
+            parmlist.append(parameters)
+            y2 = [target_fun(i, parameters[0], parameters[1],
+                             parameters[2], parameters[3]) for i in x]
+            if np.var([data, y2]) == 'nan':
+                variance.append(1e9)
+            else:
+                variance.append(np.var([data, y2]))
+        idx = np.argmin(variance)
+        parameters = parmlist[idx]
+    return parameters, err
---
+++ test.py
@@ -0,0 +1,238 @@
+import numpy as np
+import copy
+import matplotlib.pyplot as plt
+import fit
+
+
+def main():
+    # test_exp_Fit_1D()
+    # test_exp_FitX_2D()
+    # test_exp_FitY_2D()
+    # test_linear_Fit_1D()
+    # test_linear_FitX_2D()
+    # test_linear_FitY_2D()
+    print(1)
+
+
+def test_exp_Fit_1D():
+    # 拟合指数曲线解析式
+    def target_func(x, a, b, c):
+        return a * np.exp(b*x)+c
+    # 模拟生成一组实验数据(加噪声)
+    x = np.arange(0, 100, 0.2)
+    y = 2.0*np.exp(-x / 51.3)+3.0
+    noise = np.random.uniform(0, 0.1, len(x))
+    y += noise
+    fig, ax = plt.subplots()
+    ax.plot(x, y, 'b--')
+    # 调用exp_Fit_1D拟合实验数据
+    parameters, err = fit.exp_Fit_1D(x, y)
+    y_fit = [target_func(a, *parameters) for a in x]
+    print(parameters)
+    # 画图
+    ax.plot(x, y_fit, 'g')
+    plt.show()
+
+
+def test_exp_FitX_2D():
+    # 拟合指数曲线解析式
+    def target_func(x, a, b, c):
+        return a * np.exp(b*x)+c
+    # 模拟生成4组实验数据(加噪声)
+    x = np.arange(0, 100, 0.2)
+    y0 = 1.0*np.exp(-0.02*x)+2.0
+    noise = np.random.uniform(0, 0.1, len(x))
+    y0 += noise
+
+    y1 = -2*np.exp(-0.05*x)-1.0
+    noise = np.random.uniform(0, 0.1, len(x))
+    y1 += noise
+
+    y2 = -3*np.exp(+0.02*x)-1.0
+    noise = np.random.uniform(0, 0.1, len(x))
+    y2 += noise
+
+    y3 = np.exp(+0.02*x)+3.0
+    noise = np.random.uniform(0, 0.1, len(x))
+    y3 += noise
+    # 调用exp_FitX_2D拟合实验数据
+    parameters, err = fit.exp_FitX_2D([x, x, x, x], [y0, y1, y2, y3])
+    y0_fit = [target_func(a, *parameters[0]) for a in x]
+    y1_fit = [target_func(a, *parameters[1]) for a in x]
+    y2_fit = [target_func(a, *parameters[2]) for a in x]
+    y3_fit = [target_func(a, *parameters[3]) for a in x]
+    # 画图
+    plt.figure()
+    plt.subplot(2, 2, 1)
+    plt.plot(x, y0, 'b--')
+    plt.plot(x, y0_fit, 'g')
+    plt.subplot(2, 2, 2)
+    plt.plot(x, y1, 'b--')
+    plt.plot(x, y1_fit, 'g')
+    plt.subplot(2, 2, 3)
+    plt.plot(x, y2, 'b--')
+    plt.plot(x, y2_fit, 'g')
+    plt.subplot(2, 2, 4)
+    plt.plot(x, y3, 'b--')
+    plt.plot(x, y3_fit, 'g')
+    plt.show()
+
+
+def test_exp_FitY_2D():
+    # 拟合指数曲线解析式
+    def target_func(x, a, b, c):
+        return a * np.exp(b*x)+c
+    # 模拟生成4组实验数据(加噪声)
+    x = np.arange(0, 100, 0.2)
+    y0 = 1.0*np.exp(-0.02*x)+2.0
+    noise = np.random.uniform(0, 0.1, len(x))
+    y0 += noise
+
+    y1 = -2*np.exp(-0.05*x)-1.0
+    noise = np.random.uniform(0, 0.1, len(x))
+    y1 += noise
+
+    y2 = -3*np.exp(+0.02*x)-1.0
+    noise = np.random.uniform(0, 0.1, len(x))
+    y2 += noise
+
+    y3 = np.exp(+0.02*x)+3.0
+    noise = np.random.uniform(0, 0.1, len(x))
+    y3 += noise
+    # 调用exp_FitY_2D拟合实验数据
+    y_data = np.transpose(np.array([x, x, x, x])).tolist()
+    data = np.transpose(np.array([y0, y1, y2, y3])).tolist()
+    parameters, err = fit.exp_FitY_2D(y_data, data)
+    y0_fit = [target_func(a, *parameters[0]) for a in x]
+    y1_fit = [target_func(a, *parameters[1]) for a in x]
+    y2_fit = [target_func(a, *parameters[2]) for a in x]
+    y3_fit = [target_func(a, *parameters[3]) for a in x]
+    # 画图
+    plt.figure()
+    plt.subplot(2, 2, 1)
+    plt.plot(x, y0, 'b--')
+    plt.plot(x, y0_fit, 'g')
+    plt.subplot(2, 2, 2)
+    plt.plot(x, y1, 'b--')
+    plt.plot(x, y1_fit, 'g')
+    plt.subplot(2, 2, 3)
+    plt.plot(x, y2, 'b--')
+    plt.plot(x, y2_fit, 'g')
+    plt.subplot(2, 2, 4)
+    plt.plot(x, y3, 'b--')
+    plt.plot(x, y3_fit, 'g')
+    plt.show()
+
+
+def test_linear_Fit_1D():
+    # 拟合直线曲线解析式
+    def target_func(x, a, b):
+        return a * x+b
+    # 模拟生成一组实验数据(加噪声)
+    x = np.arange(0, 100, 0.2)
+    y = 2.0*x+3
+    noise = np.random.uniform(0, 0.1, len(x))
+    y += noise
+    fig, ax = plt.subplots()
+    ax.plot(x, y, 'b--')
+    # 调用exp_Fit_1D拟合实验数据
+    parameters, err = fit.linear_Fit_1D(x, y)
+    y_fit = [target_func(a, *parameters) for a in x]
+    print(parameters)
+    # 画图
+    ax.plot(x, y_fit, 'g')
+    plt.show()
+
+
+def test_linear_FitX_2D():
+    # 拟合直线曲线解析式
+    def target_func(x, a, b):
+        return a * x+b
+    # 模拟生成4组实验数据(加噪声)
+    x = np.arange(0, 100, 0.2)
+    y0 = 1.0*x+2.0
+    noise = np.random.uniform(0, 0.1, len(x))
+    y0 += noise
+
+    y1 = x-1.0
+    noise = np.random.uniform(0, 0.1, len(x))
+    y1 += noise
+
+    y2 = -3*x-1.0
+    noise = np.random.uniform(0, 0.1, len(x))
+    y2 += noise
+
+    y3 = -2*x+3.0
+    noise = np.random.uniform(0, 0.1, len(x))
+    y3 += noise
+    # 调用exp_FitX_2D拟合实验数据
+    parameters, err = fit.linear_FitX_2D([x, x, x, x], [y0, y1, y2, y3])
+    y0_fit = [target_func(a, *parameters[0]) for a in x]
+    y1_fit = [target_func(a, *parameters[1]) for a in x]
+    y2_fit = [target_func(a, *parameters[2]) for a in x]
+    y3_fit = [target_func(a, *parameters[3]) for a in x]
+    # 画图
+    plt.figure()
+    plt.subplot(2, 2, 1)
+    plt.plot(x, y0, 'b--')
+    plt.plot(x, y0_fit, 'g')
+    plt.subplot(2, 2, 2)
+    plt.plot(x, y1, 'b--')
+    plt.plot(x, y1_fit, 'g')
+    plt.subplot(2, 2, 3)
+    plt.plot(x, y2, 'b--')
+    plt.plot(x, y2_fit, 'g')
+    plt.subplot(2, 2, 4)
+    plt.plot(x, y3, 'b--')
+    plt.plot(x, y3_fit, 'g')
+    plt.show()
+
+
+def test_linear_FitY_2D():
+    # 拟合直线曲线解析式
+    def target_func(x, a, b):
+        return a * x+b
+    # 模拟生成4组实验数据(加噪声)
+    x = np.arange(0, 100, 0.2)
+    y0 = 1.0*x+2.0
+    noise = np.random.uniform(0, 0.1, len(x))
+    y0 += noise
+
+    y1 = x-1.0
+    noise = np.random.uniform(0, 0.1, len(x))
+    y1 += noise
+
+    y2 = -3*x-1.0
+    noise = np.random.uniform(0, 0.1, len(x))
+    y2 += noise
+
+    y3 = -2*x+3.0
+    noise = np.random.uniform(0, 0.1, len(x))
+    y3 += noise
+    # 调用exp_FitY_2D拟合实验数据
+    y_data = np.transpose(np.array([x, x, x, x])).tolist()
+    data = np.transpose(np.array([y0, y1, y2, y3])).tolist()
+    parameters, err = fit.linear_FitY_2D(y_data, data)
+    y0_fit = [target_func(a, *parameters[0]) for a in x]
+    y1_fit = [target_func(a, *parameters[1]) for a in x]
+    y2_fit = [target_func(a, *parameters[2]) for a in x]
+    y3_fit = [target_func(a, *parameters[3]) for a in x]
+    # 画图
+    plt.figure()
+    plt.subplot(2, 2, 1)
+    plt.plot(x, y0, 'b--')
+    plt.plot(x, y0_fit, 'g')
+    plt.subplot(2, 2, 2)
+    plt.plot(x, y1, 'b--')
+    plt.plot(x, y1_fit, 'g')
+    plt.subplot(2, 2, 3)
+    plt.plot(x, y2, 'b--')
+    plt.plot(x, y2_fit, 'g')
+    plt.subplot(2, 2, 4)
+    plt.plot(x, y3, 'b--')
+    plt.plot(x, y3_fit, 'g')
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
