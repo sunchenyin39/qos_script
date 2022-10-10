2f4c749ed9f320efd2568e7bad047f69e69e4d07
PhyEM_Chenyin <sunchenyin@mail.ustc.edu.cn>
2022-10-10 11:02:58 +0800

    1.0

--- fit.py
+++ fit.py
@@ -93,40 +93,79 @@ def guess_freq(x, y):
     return guess
 
 
-def sin_Fit_1D(x, data, fitRangeMin=None, fitRangeMax=None, fitInitial=None):
-    def target_fun(x, a, b, c, phi):
-        return a * np.sin(2*np.pi*b*x + phi) + c
+# def sin_Fit_1D(x, data, fitRangeMin=None, fitRangeMax=None, fitInitial=None):
+#     def target_fun(x, a, b, c, phi):
+#         return a * np.sin(2*np.pi*b*x + phi) + c
+
+#     # make a guess if initial parameters are not available
+#     if fitInitial is None:
+#         if np.argmax(data) == len(data) - 1:
+#             freq = 1/np.pi/x[-1]
+#         else:
+#             freq = guess_freq(x, data)
+#         amp = (np.max(data)-np.min(data))/2
+#         offset = np.mean(data)
+#         fitInitial = [amp, freq, offset, -np.pi/2]
+
+#     parameters, covariance = optimize.curve_fit(
+#         target_fun, x, data, p0=fitInitial, maxfev=500000)
+#     err = np.sqrt(np.diag(covariance)) * 1.96  # 95% confidence area
+
+#     if err[1] > abs(parameters[1])*0.1:
+#         errlist = []
+#         parmlist = []
+#         variance = []
+#         for ii in range(5, len(data)):
+#             fitInitial[1] = 1.0/(2*np.pi*x[ii])
+#             parameters, covariance = optimize.curve_fit(
+#                 target_fun, x, data, p0=fitInitial, maxfev=500000)
+#             err = np.sqrt(np.diag(covariance)) * 1.96  # 95% confidence area
+#             errlist.append(err)
+#             parmlist.append(parameters)
+
+#             y2 = [target_fun(i, parameters[0], parameters[1],
+#                              parameters[2], parameters[3]) for i in x]
+#             if np.var([data, y2]) == 'nan':
+#                 variance.append(1e9)
+#             else:
+#                 variance.append(np.var([data, y2]))
+#         idx = np.argmin(variance)
+#         parameters = parmlist[idx]
+#         err = errlist[idx]
+#     return parameters, err
+
+def fit_sin(x, y, p0=None):
+    def target(t, a0, a1, a2, a3):
+        return a0 * np.sin(t / a1 + a2) + a3
 
     # make a guess if initial parameters are not available
     if p0 is None:
-        if np.argmax(data) == len(data) - 1:
+        if np.argmax(y) == len(y) - 1:
             freq = 1/np.pi/x[-1]
         else:
-            freq = guess_freq(x, data)
-        amp = (np.max(data)-np.min(data))/2
-        offset = np.mean(data)
-        p0 = [amp, freq, offset, -np.pi/2]
+            freq = guess_freq(x, y)
+        # amp = np.std(y) * 2.0 ** 0.5
+        amp = (np.max(y)-np.min(y))/2
+        offset = np.mean(y)
+        p0 = [amp, 1 / (2 * np.pi * freq), -np.pi/2, offset]
 
-    parameters, covariance = optimize.curve_fit(
-        target_fun, x, data, p0=p0, maxfev=500000)
+    parameters, covariance = optimize.curve_fit(target, x, y, p0=p0, maxfev=500000)  # maxfev修改拟合次数上限，默认为1000
     err = np.sqrt(np.diag(covariance)) * 1.96  # 95% confidence area
-    if err[1] > abs(parameters[1])*0.1:
-        errlist = []
-        parmlist = []
-        variance = []
-        for ii in range(5, len(data)):
+    if err[1]>abs(parameters[1])*0.1:
+        errlist=[]
+        parmlist=[]
+        variance=[]
+        for ii in range(5, len(y)):
             p0[1] = x[ii]
-            parameters, covariance = optimize.curve_fit(
-                target_fun, x, data, p0=p0, maxfev=500000)  # maxfev修改拟合次数上限，默认为1000
+            parameters, covariance = optimize.curve_fit(target, x, y, p0=p0, maxfev=500000)  # maxfev修改拟合次数上限，默认为1000
             err = np.sqrt(np.diag(covariance)) * 1.96  # 95% confidence area
             errlist.append(err)
             parmlist.append(parameters)
-            y2 = [target_fun(i, parameters[0], parameters[1],
-                             parameters[2], parameters[3]) for i in x]
-            if np.var([data, y2]) == 'nan':
+            y2=[target(i, parameters[0], parameters[1], parameters[2], parameters[3]) for i in x]
+            if np.var([y,y2])=='nan':
                 variance.append(1e9)
             else:
-                variance.append(np.var([data, y2]))
-        idx = np.argmin(variance)
-        parameters = parmlist[idx]
+                variance.append(np.var([y,y2]))
+        idx=np.argmin(variance)
+        parameters=parmlist[idx]
     return parameters, err
--- test.py
+++ test.py
@@ -11,6 +11,7 @@ def main():
     # test_linear_Fit_1D()
     # test_linear_FitX_2D()
     # test_linear_FitY_2D()
+    test_sin_Fit_1D()
     print(1)
 
 
@@ -28,7 +29,6 @@ def test_exp_Fit_1D():
     # 调用exp_Fit_1D拟合实验数据
     parameters, err = fit.exp_Fit_1D(x, y)
     y_fit = [target_func(a, *parameters) for a in x]
-    print(parameters)
     # 画图
     ax.plot(x, y_fit, 'g')
     plt.show()
@@ -135,10 +135,9 @@ def test_linear_Fit_1D():
     y += noise
     fig, ax = plt.subplots()
     ax.plot(x, y, 'b--')
-    # 调用exp_Fit_1D拟合实验数据
+    # 调用linear_Fit_1D拟合实验数据
     parameters, err = fit.linear_Fit_1D(x, y)
     y_fit = [target_func(a, *parameters) for a in x]
-    print(parameters)
     # 画图
     ax.plot(x, y_fit, 'g')
     plt.show()
@@ -165,7 +164,7 @@ def test_linear_FitX_2D():
     y3 = -2*x+3.0
     noise = np.random.uniform(0, 0.1, len(x))
     y3 += noise
-    # 调用exp_FitX_2D拟合实验数据
+    # 调用linear_FitX_2D拟合实验数据
     parameters, err = fit.linear_FitX_2D([x, x, x, x], [y0, y1, y2, y3])
     y0_fit = [target_func(a, *parameters[0]) for a in x]
     y1_fit = [target_func(a, *parameters[1]) for a in x]
@@ -209,7 +208,7 @@ def test_linear_FitY_2D():
     y3 = -2*x+3.0
     noise = np.random.uniform(0, 0.1, len(x))
     y3 += noise
-    # 调用exp_FitY_2D拟合实验数据
+    # 调用linear_FitY_2D拟合实验数据
     y_data = np.transpose(np.array([x, x, x, x])).tolist()
     data = np.transpose(np.array([y0, y1, y2, y3])).tolist()
     parameters, err = fit.linear_FitY_2D(y_data, data)
@@ -231,6 +230,26 @@ def test_linear_FitY_2D():
     plt.subplot(2, 2, 4)
     plt.plot(x, y3, 'b--')
     plt.plot(x, y3_fit, 'g')
+    plt.show()
+
+
+def test_sin_Fit_1D():
+    # 拟合sin曲线解析式
+    def target(t, a0, a1, a2, a3):
+        return a0 * np.sin(t / a1 + a2) + a3
+    # 模拟生成一组实验数据(加噪声)
+    x = np.arange(0, 10, 0.01)
+    y = 10.0 * np.sin((2*np.pi*1.0)*x - np.pi/2) + 3.0
+    noise = 5*np.random.uniform(0, 0.5, len(x))
+    y += noise
+    fig, ax = plt.subplots()
+    ax.plot(x, y, 'b-')
+    # 调用sin_Fit_1D拟合实验数据
+    parameters, err = fit.fit_sin(x, y)
+    y_fit = [target(a, *parameters) for a in x]
+    print(parameters)
+    # 画图
+    ax.plot(x, y_fit, 'g')
     plt.show()
 
 
